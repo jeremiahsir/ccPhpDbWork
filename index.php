c4f723e17f3c9a9adf525b1a2f8ac36e424f66f1
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-04-29 12:22:40 -0600

    added style.css and database connection working

--- index.php
+++ index.php
@@ -1,27 +1,47 @@
 <?php
 
-for($i = 1; $i <= 300; $i++){
+require_once("logIn.php");
+require_once("style.css");
 
-  if( ($i % 3 === 0) && ($i % 5 === 0) && ($i % 7 === 0)){
-    echo "fizzbuzzwoof" . "<br>";
-  }else if( ($i % 5 === 0) && ($i % 7 === 0) ){
-    echo "buzzwoof" . "<br>";
-  } else if( ($i % 3 === 0) && ($i % 7 === 0) ){
-    echo "fizzwoof" . "<br>";
-  } else if( ($i % 3 === 0) && ($i % 5 === 0) ){
-    echo "fizzbuzz" . "<br>";
-  } else if( ($i % 3 ) === 0){
-    echo "fizz" . "<br>";
-  } else if ( ($i % 5) === 0){
-    echo "buzz" . "<br>";
-  } else if(($i % 7 ) === 0){
-    echo "woof" . "<br>";
-  }
+  try {
 
-    else {
-    echo $i . "<br>";
-  }
+  // set up dsn
+  $dsn = 'mysql:host=' . $config["hostname"] . ';dbname=' . $config["database"];
+  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 
+  // set up pdo connection with database and set error atrributes
+  $pdo = new PDO($dsn, $config["username"], $config["password"], $options);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // setup query and excute
+  $query = 'SELECT userId, userName, userEmail, dateCreated FROM user';
+  $statement = $pdo->prepare($query);
+  $statement->execute();
+  $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+  // setup table and table headers
+  echo "<table>";
+  echo "<tr>";
+  echo "<th>User ID</th>";
+  echo "<th>User Name</th>";
+  echo "<th>User Email</th>";
+  echo "<th>Date Created</th>";
+  echo "</tr>";
+
+  while(($row = $statement->fetch()) !== false) {
+ echo "<tr>";
+ echo ("<td>" . $row["userId"] . "</td>");
+ echo ("<td>" . $row["userName"] . "</td>");
+ echo ("<td>" . $row["userEmail"] . "</td>");
+ echo ("<td>" . $row["dateCreated"] . "</td>");
+ echo "</tr>";
+}
+echo "</table>";  // end table
+
+$pdo = null;
+
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 
 
